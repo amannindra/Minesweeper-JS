997394b0d9859aa303f0083a36f344c15916f0ba
Aman Nindra <amannindra@gmail.com>
2024-03-30 23:27:44 -0700

    Major Updates

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Just reached 1:45, I gave up and used chatgpt to create the grid. Finally, I cre
 
 Currently 4:40 am. I basically have been working for 2 hours. I played 45 minutes of fortnite.I am going to sleep now.
 
+
+It is March 30 5 pm, I just came back from De Anza college Tour. I am going to continue with this project.
+
+It is 11pm, throughout the last few hours, I have been dozing between youtube and this project. I am proud to say that I am almost done. I just need to create the bomb count around you, flag, and make GUI alot better.
+
+By the way, all of the commented functions are failed versions of what I was trying to achieve.
+
 Sources:
 https://stackoverflow.com/questions/45208347/create-a-10x10-field-of-cells
 
@@ -22,3 +29,17 @@ https://stackoverflow.com/questions/1947263/using-an-html-button-to-call-a-javas
 https://www.101computing.net/minesweeper-in-javascript/
 
 https://codepen.io/bglaz/pen/dzrLJX
+
+https://stackoverflow.com/questions/45208347/
+
+
+https://stackoverflow.com/questions/57550082/
+
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/
+
+
+https://www.youtube.com/watch?v=AfhfAxKFP-s
+
+https://slicker.me/javascript/mine/minesweeper.htm
+
--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,17 @@
 game = document.getElementById("grid");
 reset = document.getElementById("reset");
 reveal = document.getElementById("reveal");
+state = document.getElementById("state");
+dev = document.getElementById("developer");
 var testMode = false;
 let rows = 10;
 let columns = 10;
 let count = 10;
-createGame();
-generateBombs();
+
+window.onload = function () {
+  createGame();
+  generateBombs();
+};
 function createGame() {
   num = 1;
   game.style.setProperty("--grid-rows", rows.toString());
@@ -17,8 +22,9 @@ function createGame() {
       td.classList.add("cell");
       td.textContent = "";
       td.id = x + "." + y;
-      var mine = document.createAttribute("data-mine");
-      var isRevealed = document.createAttribute("data-is-revealed");
+      td.addEventListener("click", checkTile);
+      var mine = document.createAttribute("mineData");
+      var isRevealed = document.createAttribute("revealData");
       isRevealed.value = "false";
       mine.value = "false";
       td.setAttributeNode(isRevealed);
@@ -33,12 +39,14 @@ function getRandomInt(max) {
 }
 
 function resetGame() {
-  for(let i=1;i<=rows; i++){
-    for(let j=1; j <= columns; j++){
-      window[i+"."+j].setAttribute("data-mine","false");
-      window[i+"."+j].style.backgroundColor = "red";
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= columns; j++) {
+      window[i + "." + j].setAttribute("mineData", "false");
+      window[i + "." + j].style.backgroundColor = "red";
+      window[i + "." + j].innerText = "";
     }
   }
+  state.innerText = "Play Game";
   generateBombs();
 }
 function generateBombs() {
@@ -46,34 +54,111 @@ function generateBombs() {
   for (let i = 1; i <= rows; i++) {
     for (let j = 1; j <= columns; j++) {
       var p = getRandomInt(rows * columns);
-      console.log("The random: " + p + ", the percentage: " + percentage);
       if (p <= percentage) {
-        console.log("test");
-        window[i+"."+j].style.backgroundColor = "black";
-        window[i+"."+j].setAttribute("data-mine","true");
+        window[i + "." + j].style.backgroundColor = "black";
+        window[i + "." + j].setAttribute("mineData", "true");
       }
     }
   }
 }
-function revealGame(e) {
-  for(let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= columns; j++){
-      var s = window[i+"."+j];
-      if(s.getAttribute("data-mine")=="false"){
-        for(var k = -1; k <= 1; k++) {
-          for(var l = -1; l <= 1; l++){
-            
+function checkTile(e, row, column) {
+  let tile = e.target;
+  if (tile.getAttribute("mineData") == "true") {
+    alert("You Lose!");
+    state.innerText = "You Lose!";
+  } else {
+    tile.style.backgroundColor = "green";
+    let position = tile.id.split(".");
+    let row = position[0];
+    let column = position[1];
+    row = parseInt(row);
+    column = parseInt(column);
+    var numberOfBombs = checkAround(row, column);
+    if (numberOfBombs == 0) {
+      spread(row, column);
+    } else {
+      tile.innerText = numberOfBombs;
+    }
+  }
+}
+function spread(row, column) {
+  return;
+}
+function check(row, column) {
+  //console.log(typeof row);
+  //console.log(typeof column);
+  if (window[row + "." + column].getAttribute("mineData") == "true") {
+    return true;
+  }
+  return false;
+}
+
+function checkAround(row, column) {
+  //console.log(row + "." + column);
+
+  var amount = 0;
+  for (i = -1; i < 2; i++) {
+    for (j = -1; j < 2; j++) {
+      if (i != 0 && j != 0) {
+        rx = row + i;
+        ry = column + j;
+        console.log(typeof rx);
+        console.log(typeof ry);
+        if (rx >= 0 && ry >= 0 && rx < rows && ry < columns) {
+          if (check(rx, ry)) {
+            amount += 1;
           }
         }
       }
     }
-
+    return amount;
   }
 }
+
+function revealGame(e) {
+  var num = 1;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= columns; j++) {
+      var s = window[i + "." + j];
+      if (s.getAttribute("mineData") == "true") {
+        s.innerText = "💣";
+        s.style.backgroundColor = "green";
+      } else {
+        s.innerText = num;
+      }
+      num += 1;
+    }
+  }
+}
+function a() {
+  alert("hi");
+}
+
+//Developer functions
+function devGame() {
+  console.log("Opened devGame");
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < columns; j++) {
+      console.log(window[i + "." + j]);
+      if (!window[i + "." + j].getAttribute("mineData") == "true") {
+        var numberOfBombs = checkAround(i, j);
+        if (numberOfBombs == 0) {
+          spread(i, j);
+        } else {
+          window[i + "." + j].innerText = numberOfBombs;
+        }
+      } 
+        
+    }
+  }
+}
+
+
+
+reset.addEventListener("keypress", a);
 reset.addEventListener("click", resetGame);
 reveal.addEventListener("click", revealGame);
-
-
+dev.addEventListener("click", devGame);
 /*
 function generateBombs(){
   var done = false;
@@ -138,10 +223,51 @@ function createCell(x, y) {
   td.dataset.row = x;
   td.dataset.column = y;
 }
-*/
+
+
+/*
+function checkAround(row, column) {
+  var amount = 0;
+  if (check(row + 1, column)) {
+    console.log("top");
+    amount += 1;
+  }
+  if (check(row + 1, column - 1)) {
+    console.log("top left");
+    amount += 1;
+  }
+  if (check(row + 1, column + 1)) {
+    console.log("top right");
+    amount += 1;
+  }
+  if (check(row - 1, column)) {
+    console.log("bottom");
+    amount += 1;
+  }
+  if (check(row - 1, column - 1)) {
+    console.log("bottom left");
+    amount += 1;
+  }
+  if (check(row - 1, column + 1)) {
+    console.log("bottom right");
+    amount += 1;
+  }
+  if (check(row, column + 1)) {
+    console.log("right");
+    amount += 1;
+  }
+  if (check(row, column - 1)) {
+    console.log("left");
+    amount += 1;
+  }
+  return amount;
+}*/
+
 /* Sources
 
 https://stackoverflow.com/questions/45208347/create-a-10x10-field-of-cells
 https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
-
+https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_flexible_box_layout/Aligning_items_in_a_flex_container
+https://www.youtube.com/watch?v=AfhfAxKFP-s
+https://slicker.me/javascript/mine/minesweeper.htm
 */
